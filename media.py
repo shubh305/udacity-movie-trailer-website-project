335b3b37491d5f9f0834efdb07b90fe9fae307c4
shubh305 <shubham.305singh@gmail.com>
2017-03-03 21:46:49 +0530

    Updated code as per the PEP8 standards.

--- media.py
+++ media.py
@@ -1,13 +1,17 @@
 import webbrowser
 
+
 class Movie():
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, movie_release_date):
+
+    # A data structure to store the movies and their required details
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, movie_release_date):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.release_date = movie_release_date
-        
+
+    # A data structure to open the trailer when click on the poster
     def show_trailer(self):
         webbrowser.open(self.trailer)
